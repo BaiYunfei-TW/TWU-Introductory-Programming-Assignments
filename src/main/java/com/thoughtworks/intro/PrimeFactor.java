6426dd4e7071252b0d5a9914c98cf7823cb39ad4
BaiYunfei-TW <baiyunfei_tw@foxmail.com>
2018-07-11 22:06:52 +0800

    implemented Prime Factors Exercise

--- src/main/java/com/thoughtworks/intro/PrimeFactor.java
+++ src/main/java/com/thoughtworks/intro/PrimeFactor.java
@@ -1,4 +1,21 @@
 package com.thoughtworks.intro;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class PrimeFactor {
+    public List<Integer> generate(int n) {
+        List<Integer> resultList = new ArrayList<>();
+        int prime = 2;
+        while (n != 1){
+            while (n % prime == 0){
+                resultList.add(prime);
+
+                n = n / prime;
+            }
+            prime++;
+        }
+        return resultList;
+    }
 }
--- src/test/java/com/thoughtworks/intro/TestPrimeFactor.java
+++ src/test/java/com/thoughtworks/intro/TestPrimeFactor.java
@@ -1,4 +1,45 @@
 package com.thoughtworks.intro;
 
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class TestPrimeFactor {
+
+    private PrimeFactor primeFactor = new PrimeFactor();
+
+    @Test
+    public void should_return_result_list_when_given_30() {
+        Integer[] resultArr = {2, 3, 5};
+        List<Integer> excepted = Arrays.asList(resultArr);
+
+        List<Integer> actual = primeFactor.generate(30);
+
+        assertThat(actual, equalTo(excepted));
+    }
+
+    @Test
+    public void should_return_result_list_when_given_10() {
+        Integer[] resultArr = {2, 5};
+        List<Integer> excepted = Arrays.asList(resultArr);
+
+        List<Integer> actual = primeFactor.generate(10);
+
+        assertThat(actual, equalTo(excepted));
+    }
+
+    @Test
+    public void should_return_result_list_when_given_66() {
+        Integer[] resultArr = {2,3,11};
+        List<Integer> excepted = Arrays.asList(resultArr);
+
+        List<Integer> actual = primeFactor.generate(66);
+
+        assertThat(actual, equalTo(excepted));
+    }
+
 }
